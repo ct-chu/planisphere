5a432936b3a133da2c6d690cc0ef8d93ffa80e2a
ct-chu <einsteinchu@gmail.com>
2024-04-17 09:23:34 +0800

    fix starchart position to parent div, move right panel to module, format code

--- src/app/page.js
+++ src/app/page.js
@@ -20,11 +20,11 @@ const darkTheme = createTheme({
 export default function Home() {
 
   const [rotateDeg, setRotateDeg] = useState(0)
-  
-  const months = ["1月 JAN","2月 Feb","3月 Mar","4月 Apr","5月 May","6月 Jun","7月 Jul","8月 Aug","9月 Sep","10月 Oct","11月 Nov","12月 Dec"]
+
+  const months = ["1月 JAN", "2月 Feb", "3月 Mar", "4月 Apr", "5月 May", "6月 Jun", "7月 Jul", "8月 Aug", "9月 Sep", "10月 Oct", "11月 Nov", "12月 Dec"]
   const monthOffsetValues = [0, 30.49, 58.11, 88.65, 118.33, 148.86, 178.52, 209.04, 239.64, 269.15, 299.78, 329.37]
   const dayDeg = 0.98
-  const days31 = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
+  const days31 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
   const times = ["6:00 PM", "6:15 PM", "6:30 PM", "6:45 PM", "7:00 PM", "7:15 PM", "7:30 PM", "7:45 PM", "8:00 PM", "8:15 PM", "8:30 PM", "8:45 PM", "9:00 PM", "9:15 PM", "9:30 PM", "9:45 PM", "10:00 PM", "10:15 PM", "10:30 PM", "10:45 PM", "11:00 PM", "11:15 PM", "11:30 PM", "11:45 PM", "12:00 AM", "12:15 AM", "12:30 AM", "12:45 AM", "1:00 AM", "1:15 AM", "1:30 AM", "1:45 AM", "2:00 AM", "2:15 AM", "2:30 AM", "2:45 AM", "3:00 AM", "3:15 AM", "3:30 AM", "3:45 AM", "4:00 AM", "4:15 AM", "4:30 AM", "4:45 AM", "5:00 AM", "5:15 AM", "5:30 AM", "5:45 AM", "6:00 AM"]
   const timeOffsetValues = {
     "6:00 PM": 89.4,
@@ -90,13 +90,13 @@ export default function Home() {
   // let root = document.documentElement.starchart
 
   const rotate10deg = () => {
-    setRotateDeg(rotateDeg+10)
+    setRotateDeg(rotateDeg + 10)
     // root.style.setProperty("--starchart-rotation", rotateDeg, "deg")
     console.log(rotateDeg)
   }
 
   const rotatem10deg = () => {
-    setRotateDeg(rotateDeg-10)
+    setRotateDeg(rotateDeg - 10)
     // root.style.setProperty("--starchart-rotation", rotateDeg, "deg")
     console.log(rotateDeg)
   }
@@ -104,12 +104,12 @@ export default function Home() {
   const setDaysInMonth = (month) => {
     let dayInM = days31
     console.log("month chosen")
-    if ((month == "4月 Apr") || (month == "6月 Jun") || (month == "9月 Sep") ||  (month == "11月 Nov")) {
+    if ((month == "4月 Apr") || (month == "6月 Jun") || (month == "9月 Sep") || (month == "11月 Nov")) {
       dayInM.pop()
       setDays(dayInM)
     } else if (month == "2月 Feb") {
       for (let i = 0; i < 3; i++) {
-        dayInM.pop()    
+        dayInM.pop()
       }
       setDays(dayInM)
       console.log(dayInM)
@@ -119,8 +119,8 @@ export default function Home() {
   }
 
   const rotateToTime = () => {
-    let selectedMonthOffset = -1* monthOffsetValues[months.indexOf(month)]
-    let selectedDayOffset = -1* (Number(day) - 1) * dayDeg
+    let selectedMonthOffset = -1 * monthOffsetValues[months.indexOf(month)]
+    let selectedDayOffset = -1 * (Number(day) - 1) * dayDeg
     let selectedTimeOffset = timeOffsetValues[time]
     // let selectedTimeOffset = 0
     setRotateStyle("1s ease-in-out")
@@ -132,8 +132,8 @@ export default function Home() {
 
   const rotateAnimation = () => {
     console.log("rotate animation pressed")
-    setRotateStyle("20s linear")
-    setRotateDeg(-360)
+    setRotateStyle("24s linear")
+    setRotateDeg(rotateDeg - 360)
     // console.log(rotating)
     // let myVar = !rotating
     // setRotating(myVar)
@@ -142,45 +142,18 @@ export default function Home() {
     //   console.log("start rotating")
     //   setRotateStyle("20s linear")
     //   setRotateDeg(-360)
-      // setRotateStyle(0)
-      // setRotateDeg(0)
-      // setrotateStyle(1)
-      // setRotating(!rotating)
+    // setRotateStyle(0)
+    // setRotateDeg(0)
+    // setrotateStyle(1)
+    // setRotating(!rotating)
     // } else {
     //   setRotateStyle("1s ease-in-out")
     // }
   }
 
- return (
-  <ThemeProvider theme={darkTheme}>
-    <CssBaseline />
-   <div className={styles.container}>
-    <Grid container
-      direction="row"
-      spacing={2}
-      sx={{
-        height: "100vh",
-        width: "100%",
-      }}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Grid xs={8}
-        sx={{
-          height: "90vh",
-          width : "90vh"
-        }}
-      >
-        <div className={styles.starchart}>
-          <img height="100%" style={{transition: rotateStyle, rotate: `${rotateDeg}deg`}} src={yellowStarchart}/>
-          <div className={styles.overlay}>
-            <img height="100%" width="100%" src={jacket}/>
-          </div>
-        </div>
-      </Grid>
-      <Grid 
-        xs={2}
-      >
+  const RightPanel = () => {
+    return (
+      <Grid>
         <Autocomplete
           disablePortal
           selectOnFocus
@@ -190,7 +163,7 @@ export default function Home() {
             setMonth(newValue)
             setDaysInMonth(newValue)
           }}
-          sx={{width:200}}
+          sx={{ width: 200 }}
           renderInput={(params) => <TextField {...params} label="月 Month" />}
         />
         <br />
@@ -202,7 +175,7 @@ export default function Home() {
           onChange={(event, newValue) => {
             setDay(newValue)
           }}
-          sx={{width:200}}
+          sx={{ width: 200 }}
           renderInput={(params) => <TextField {...params} label="日 Day" />}
         />
         <br />
@@ -214,38 +187,75 @@ export default function Home() {
           onChange={(eventT, newTime) => {
             setTime(newTime);
           }}
-          sx={{width:200}}
+          sx={{ width: 200 }}
           renderInput={(displayTime) => <TextField {...displayTime} label="時 Time" />}
         />
         <br />
-         <Button
+        <Button
           variant="contained"
           onClick={rotateToTime}
-          >
-            GO
+        >
+          GO
         </Button>
         <Button
           variant="contained"
           onClick={rotatem10deg}
-          >
-            &lt;
+        >
+          &lt;
         </Button>
         <Button
           variant="contained"
           onClick={rotate10deg}
-          >
-            &gt;
+        >
+          &gt;
         </Button>
         <br />
         <Button
           variant="contained"
           onClick={rotateAnimation}
-          >
-            Anime
+        >
+          Anime
         </Button>
       </Grid>
-    </Grid>
-   </div>
-  </ThemeProvider>
- );
+    )
+  }
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className={styles.container} maxWidth={false}>
+        <Grid container
+          direction="row"
+          spacing={2}
+          sx={{
+            height: "100vh",
+            width: "100%",
+          }}
+          alignItems="center"
+          justifyContent="space-evenly"
+        >
+          <Grid xs={6}
+            sx={{
+              height: "95vh",
+              width: "95vh"
+            }}
+            style={{ position: "relative" }}
+          >
+            <div className={styles.starchart}>
+              <img height="100%" style={{ transition: rotateStyle, rotate: `${rotateDeg}deg` }} src={yellowStarchart} />
+              <div className={styles.overlay}>
+                <img height="100%" width="100%" src={jacket} />
+              </div>
+            </div>
+          </Grid>
+          <Grid container
+            xs={3}
+            justifyContent="center"
+          >
+            <RightPanel />
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
+  );
 }
