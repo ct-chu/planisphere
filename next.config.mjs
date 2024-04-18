b804fa5c227212e0aacf60203d5f543f015443c3
ct-chu <einsteinchu@gmail.com>
2024-04-18 17:26:16 +0800

    update next config for github page static paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
     output: 'export',
+    assetPrefix: isProd ? 'https://ct-chu.github.io/planisphere/' : undefined,
 };
 
 export default nextConfig;
