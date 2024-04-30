c79f928aafa81358a96e0deef08d10d46302dea8
ct-chu <einsteinchu@gmail.com>
2024-04-30 18:21:10 +0800

    add swcMinify, react strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 
 const nextConfig = {
     output: 'export',
+    reactStrictMode: true,
+    swcMinify: true,
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
